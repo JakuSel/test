e350e1255fb8211706db7a7e318b026b4aa807f3
Jacob <jakub.seliga@akademiasovy.sk>
2017-11-29 19:30:04 +0100

    adding new func

--- decrypt.cpp
+++ decrypt.cpp
@@ -5,47 +5,32 @@
 #include<stdbool.h>
 #include<ctype.h>
 
-typedef struct{
-	char origin[2];
-	char cyp[2];
-}cyper;
 int main()
 {
-	cyper arr[36];
-	FILE *fr,*fr1,*fw,*fw1;
+	FILE *fr,*fr1,*fw;
 	//otvorenie  ssuboru s klucom
-	fr=	fopen("key.txt","r");
+	fr=	fopen("codeTable.txt","r");
 	if(fr==NULL)
 	{
 		printf("File error");
 		return 1;
 	}
-	
-	char z=0,x=0,y=0;
-	int i=0;
-	//nacitanie klucu do strukturi, origin-> original písmeno, cyp-> sifra
-	while((z=fgetc(fr)) != EOF)
+	char key[80],z=0,x=0,y=0;
+	int i=0,j=0;
+
+	while ((z = fgetc(fr)) != EOF) 
 	{
-		for(i=0;i<36;i++)
+    	if (isalpha(z) || isdigit(z)) 
 		{
-		fscanf(fr,"%s%s",&arr[i].origin,&arr[i].cyp);
-		
-		}					
-	}
-	fclose(fr);
-	//vypis struktury
-	for(i=0;i<36;i++)
-	{
-		printf("%s %s ",arr[i].origin,arr[i].cyp);
-	}
-	printf("\n");
-	printf("%s%s ",arr[0].origin,arr[0].cyp);
-	//po tade to ide, vypise mi strukturu a každu jednu poziciu
+     	key[i] = z;
+     	i++;
+   		}
+ 	}
+ 	fclose(fr);
 	
-	//otvorim dekodovaci subor a subor kde ten dekodovaci text zmenim
-	fr1=fopen("encodedText.txt","r");
-	fw=fopen("ans.txt","rw");
-	fw1=fopen("ans1.txt","w");
+	fr1=fopen("secretCode.txt","r");
+	fw=fopen("ans.txt","w");
+	
 	if(fr1==NULL)
 	{
 		printf("File error");
@@ -56,34 +41,24 @@ int main()
 		printf("File error");
 		return 1;
 	}
-	if(fw1==NULL)
-	{
-		printf("File error");
-		return 1;
-	}
-	//menim dekodavany subor na ciste znaky a zapisujem ich do suboru
+
+	
 	while((x=fgetc(fr1)) != EOF)
 	{
-		//ak to nie je pismeno alebo cislo tak idem dalej, continue
-		if(isalnum(x)==0)
-		continue;
-		else
+		if(isalnum(toupper(x)))
 		{
-		fputc(toupper(x),fw);
-	//a tu som chcel aby znaky z predosleho suboru dalo do dalšieho suboru, kde by ich kontrolovalo zo strukturov a 
-	//zapísalo by len znak ktory patri origin znaku
-	while(y=fgetc(fw)!=EOF)
-	{
-	for(i=0;i<36;i++)
-	{
-		if (arr[i].origin=y)
-		y=arr[i].cyp;
-	}
-		fputc(y,fw);
+			for(j=0;j<i;j=j+2)
+			{
+				if(toupper(x)==key[j])
+				x=key[j+1];
+				break;
+			}
+			
+		}
 		
-	} 		
-	
-	}
+		fputc(x,fw);
+			
+		
 	}
 	fclose(fr1);
 	fclose(fw);
